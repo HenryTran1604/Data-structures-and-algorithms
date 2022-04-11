8bf47a2138ad551d5c323a3fa697e2eab3aef7cb
Trần Quang Huy <100793906+HenryTran1604@users.noreply.github.com>
2022-04-11 15:40:40 +0700

    DSA02020: SỐ NHỎ NHẤT CÓ N ƯỚC SỐ

--- DSA02020_SoNhoNhatCoNUocSo.cpp
+++ DSA02020_SoNhoNhatCoNUocSo.cpp
@@ -1,29 +1,20 @@
 #include <bits/stdc++.h>
-#define fi first
-#define sec second
-#define pii pair<int, int>
-#define toint(a) a-'0'
-#define all(a) a.begin(), a.end()
-#define endl '\n'
 using namespace std;
 using ll = long long;
 const int MOD = 1e9+7;
+int n; ll ans;
+int pr[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
 
-bool isprime(int);
-int n; ll ans = 1e18;
-ll pr[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
-void Try(int i, int soUoc, ll kq){
-	if(soUoc > n || kq > ans)
+void Try(int i, int SoUoc, ll res){
+	if(SoUoc > n)
 		return;
-	if(soUoc == n){
-		ans = min(ans, kq);
-		return;
+	if(SoUoc == n){
+		ans = min(ans, res);
 	}
-		
-	for(int j = 1; j < n; j++){
-		kq *= pr[i];
-		if(kq > ans) break;
-		Try(i+1, soUoc*(j+1), kq);
+	for(int j = 1; ; j++){
+		if(res*pr[i] > ans) break;
+		res *= pr[i];
+		Try(i+1, SoUoc*(j+1), res);
 	}
 }
 int main()
@@ -31,23 +22,15 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int test; test = 1;
-	while(test--){
+	int test; cin >> test;
+	while(test--)
+	{
 		cin >> n;
+		ans = 1e18;
 		Try(0, 1, 1);
-		cout << ans;
+		cout << ans << endl;
 	}
 	return 0;
-}
-bool isprime(int n){
-	if(n < 2) return 0;
-	if(n < 4) return 1;
-	if(n%2==0 || n%3==0) return 0;
-	int tmp = sqrt(n);
-	for(int i = 5; i <= tmp; i+=6){
-		 if(n%i==0 || n%(i+2)==0) return 0;
-	}
-	return 1;
 }
 
 /*Code by: Tran Quang Huy*/
